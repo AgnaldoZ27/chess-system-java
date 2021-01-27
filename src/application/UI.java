99d93edcdde70ea54c1ea3e98d540c39d4165977
AgnaldoZ27 <agnaldojose.131@gmail.com>
2021-01-27 10:32:22 -0400

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,11 +55,17 @@ public class UI {
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn : "+ chessMatch.getTurn());
-		System.out.println("Waiting player: "+ chessMatch.getCurrentPlayer());
-		
-		if(chessMatch.getCheck()){
-			System.out.println("CHECK!");
+		System.out.println("Turn : " + chessMatch.getTurn());
+
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: "+ chessMatch.getCurrentPlayer());
 		}
 	}
 
@@ -101,10 +107,12 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 		System.out.println("captured pieces:");
 		System.out.print("white: ");
 		System.out.print(ANSI_WHITE);
@@ -115,5 +123,5 @@ public class UI {
 		System.out.print(Arrays.toString(black.toArray()));
 		System.out.println(ANSI_RESET);
 	}
-	
+
 }
