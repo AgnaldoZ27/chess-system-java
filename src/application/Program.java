122a46c91a1709b4d380e5b8f45b0bf30c8505f9
AgnaldoZ27 <agnaldojose.131@gmail.com>
2021-02-09 16:34:48 -0400

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,17 +34,21 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(scan);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-				if(capturedPiece != null) {
+
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
-				if(chessMatch.getPromoted() != null) {
+
+				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = scan.nextLine();
+					String type = scan.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid velue! Enter piece for promotion (B/N/R/Q): ");
+						type = scan.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
-				
+
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				scan.nextLine();
